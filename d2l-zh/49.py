9326edff7a74a0ab5bf79df326750cf27f1784b3
YuanwenFu <YuanwenFu98@163.com>
2026-03-17 18:04:13 +0800

    20260317-18-03 submit

---
+++ d2l-zh/49.py
@@ -0,0 +1,20 @@
+import os 
+import time 
+import sys 
+import torch 
+import torchvision 
+from d2l import torch as d2l 
+
+def func1():
+    """
+    样式迁移
+    """
+    return 
+
+if __name__ == "__main__":
+    print("start...")
+    t1 = time.time()
+    func1()
+    t2 = time.time()
+    print(f"Time taken: {t2 - t1} seconds")
+    print("end...")
